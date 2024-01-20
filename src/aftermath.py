52ee08eba1881ea02a691affff09043b77d243ec
Benjamin Mugnier <mugnier.benjamin@gmail.com>
2024-01-20 18:37:05 +0100

    Remove useless aftermath player member

--- src/aftermath.py
+++ src/aftermath.py
@@ -3,7 +3,6 @@ from typing import Any
 class Aftermath():
     #TODO either indian or player for source and target
     def __init__(self, source: Any, target: Any, damage: int) -> None:
-        self.players: List[Player] = []
         self.source = source
         self.target = target
         self.damage = damage
