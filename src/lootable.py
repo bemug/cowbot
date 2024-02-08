57b641ca7d1ce3af4c5c88ece3e8a6ea362b2afe
Benjamin Mugnier <mugnier.benjamin@gmail.com>
2024-02-08 20:03:33 +0100

    Add lootable list

--- src/lootable.py
+++ src/lootable.py
@@ -25,10 +25,29 @@ class Lootable():
 
 lootables = (
         #Weapons
-        Lootable("Gun 1", Weapon, PeakCurve(0,0,5), [2,0], [PeakCurve(0,0,3)]),
-        Lootable("Gun 2", Weapon, PeakCurve(0,0,5), [1,0], [PeakCurve(0,0,3), PeakCurve(0,0,3)]),
-        Lootable("Gun 2", Weapon, PeakCurve(0,0,5), [1,0], [PeakCurve(0,0,3), PeakCurve(0,0,3)]),
-        Lootable("Gun 2", Weapon, PeakCurve(0,0,5), [1,0], [PeakCurve(0,0,3), PeakCurve(0,0,3)]),
-        Lootable("Gun 2", Weapon, PeakCurve(0,0,5), [1,0], [PeakCurve(0,0,3), PeakCurve(0,0,3)]),
-        #Armors
+        #Require at least level 2 to spawn
+        Lootable("Pistolet de poche Derringer", Weapon, PeakCurve(2,0,5), [1], [PeakCurve(0,0,3)]),
+        Lootable("Revolver Colt", Weapon, PeakCurve(2,0,5), [1,0], [PeakCurve(0,0,3), PeakCurve(0,0,3)]),
+        Lootable("Revolver Remington", Weapon, PeakCurve(0,0,5), [1,0], [PeakCurve(0,0,3), PeakCurve(0,0,3)]),
+        Lootable("Revolver S&W", Weapon, PeakCurve(0,0,5), [1,0], [PeakCurve(0,0,3), PeakCurve(0,0,3)]),
+        Lootable("Pistolet Volcanic", Weapon, PeakCurve(0,0,5), [1,0], [PeakCurve(0,0,3), PeakCurve(0,0,3)]),
+        Lootable("Fusil Spencer", Weapon, PeakCurve(0,0,5), [1,0], [PeakCurve(0,0,3), PeakCurve(0,0,3)]),
+        Lootable("Fusil Sharps", Weapon, PeakCurve(0,0,5), [1,0], [PeakCurve(0,0,3), PeakCurve(0,0,3)]),
+        Lootable("Fusil Springfield", Weapon, PeakCurve(0,0,5), [1,0], [PeakCurve(0,0,3), PeakCurve(0,0,3)]),
+        Lootable("Carabine Mauser", Weapon, PeakCurve(0,0,5), [1,0], [PeakCurve(0,0,3), PeakCurve(0,0,3)]),
+        Lootable("Carabine Winchester", Weapon, PeakCurve(0,0,5), [1,0], [PeakCurve(0,0,3), PeakCurve(0,0,3)]),
+        Lootable("Mitrailleuse Gatling", Weapon, PeakCurve(10,25,25,0.1), [0,40], [PeakCurve(0,0,1), PeakCurve(0,0,20)]),
+        ### Armors
+        #Require at least level 4 to spawn
+        Lootable("Open crease", Weapon, PeakCurve(4,4,5), [1], [PeakCurve(0,0,3)]),
+        Lootable("Cattleman crease", Weapon, PeakCurve(4,4,5), [1,0], [PeakCurve(0,0,3), PeakCurve(0,0,3)]),
+        Lootable("Cutter crease", Weapon, PeakCurve(3,0,5), [1,0], [PeakCurve(0,0,3), PeakCurve(0,0,3)]),
+        Lootable("Ridgetop crease", Weapon, PeakCurve(3,0,5), [1,0], [PeakCurve(0,0,3), PeakCurve(0,0,3)]),
+        Lootable("Dakota crease", Weapon, PeakCurve(3,0,5), [1,0], [PeakCurve(0,0,3), PeakCurve(0,0,3)]),
+        Lootable("Montana crease", Weapon, PeakCurve(3,0,5), [1,0], [PeakCurve(0,0,3), PeakCurve(0,0,3)]),
+        Lootable("Gus crease", Weapon, PeakCurve(3,0,5), [1,0], [PeakCurve(0,0,3), PeakCurve(0,0,3)]),
+        Lootable("Brick crease", Weapon, PeakCurve(3,0,5), [1,0], [PeakCurve(0,0,3), PeakCurve(0,0,3)]),
+        Lootable("Diamond crease", Weapon, PeakCurve(3,0,5), [1,0], [PeakCurve(0,0,3), PeakCurve(0,0,3)]),
+        Lootable("Telescope crease", Weapon, PeakCurve(3,0,5), [1,0], [PeakCurve(0,0,3), PeakCurve(0,0,3)]),
+        Lootable("Gambler crease", Weapon, PeakCurve(12,25,25), [0,20], [PeakCurve(0,0,1), PeakCurve(0,0,10)]),
     )
