b741a238a0cbac797f6c412bba3d4554bd0babb0
bemug <mugnier.benjamin@gmail.com>
2024-05-13 13:17:25 +0200

    Buff foe scaling

--- cowbot/foe.py
+++ cowbot/foe.py
@@ -8,7 +8,7 @@ class Foe(Character):
     animals_file: str = "data/animals.txt"
     adjectives_file: str = "data/adjectives.txt"
     #Foe level up faster than players
-    scale_factor = 0.65
+    scale_factor = 0.5
 
     def __init__(self, exp: int) -> None:
         super().__init__()
